0cf2fc1f10675aeee7ede41c6c49335124578842
Shashwat Srivastava <darklrd@gmail.com>
2012-11-23 18:19:59 +0530

    iFlyChat PHP Client - requires jQuery

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once('./iflychat_api.php');
 $user_details = array('name' => 'admin', 'id' => '1', 'is_admin' => TRUE,);
 $ifly_code = iflychat_render_chat($user_details);
-echo '<html><head><script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>'; 
+echo '<html><head><script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>'; 
 print $ifly_code;
 echo '</head><body><h1>TEST.......................................................................................................................</h1><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><h1>TEST</h1></body></html>';
 ?>
