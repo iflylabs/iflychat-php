2b83095006ab27cc05185da4ca5a49679dae61fb
Shashwat Srivastava <darklrd@gmail.com>
2012-11-23 18:56:49 +0530

    iFlyChat PHP Client - settings added

--- iflychat_api_settings.php
+++ iflychat_api_settings.php
@@ -5,17 +5,32 @@ global $_iflychat, $iflychat;
 $iflychat = array(
 /* Get a valid API key from following instructions given here - https://iflychat.com/installation-php-client */
   'api_key' => 'XXXXXXX',
-/* The relative path from home URL to where this file is present */  
+/* The relative path from home or base URL to where this file is present */  
   'path' => '/iflychat-php/',
+/* The base url, do not change */
   'base' => (($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'],
+/* The theme of the chat. Valid values are 'light' and 'dark'. */  
   'theme' => 'light',
+/* Display user avatars or pictures. Valid values are TRUE and FALSE. */
   'user_picture' => TRUE,
+/* Enable sound notification by default. Valid values are TRUE and FALSE. */  
   'notification_sound' => TRUE,
+/* Enable emoticons. Valid values are TRUE and FALSE. */
   'enable_smileys' => TRUE,
+/* Enable logging of chat messages. Valid values are TRUE and FALSE. */
   'log_chat' => TRUE,
+/* Prefix to be used with anonymous users (4 to 7 characters) */
   'anon_prefix' => 'Guest',
+/* Select whether to use random generated name or number to assign to a new anonymous user. 
+   Valid values are TRUE and FALSE.
+   TRUE - Use random names
+   FALSE - Use random numbers   */
   'anon_use_name' => TRUE,
+/* Select country to use for generating names of anonymous users. 
+   Valid values are available at this link - http://api.iflychat.com/anam/c/. 
+   It should be of 3 digit. */
   'anon_name_set' => 'usa',
+/* Enable public chatroom. Valid values are TRUE and FALSE. */
   'public_chatroom' => TRUE,
   'chat_top_bar_color' => '#222222',
   'chat_top_bar_text_color' => '#FFFFFF',
@@ -27,7 +42,6 @@ $iflychat = array(
   'stop_links' => '1',
   'allow_anon_links' => TRUE,
   'show_admin_list' => FALSE,
-  
 );
 
 $_iflychat = array (
